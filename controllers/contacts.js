0ae392710e61e0b25568ca1acccf599ffce3f6d1
vitalyon <vitalyon@gmail.com>
2023-10-26 17:47:10 +0200

    auth_5

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,64 +1,71 @@
 const { Contact } = require("../models/contact");
 const { HttpError } = require("../helpers/index");
+
 const ctrlWrapper = require("../decorators/ctrlWrapper");
 
-const getAll = async (reg, res) => {
-    const result = await Contact.find();
-    res.json(result);
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
+  res.json(result);
 };
 
 const getById = async (req, res) => {
-    const { id } = req.params;
-    const result = await Contact.findById(id);
-    if (!result) 
-    {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findById({ _id: id, owner });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
 };
 
-const add = async (reg, res) => {
-    const result = await Contact.create(reg.body);
-    res.status(201).json(result);
+const add = async (req, res) => {
+    const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
 };
 
-const updateById = async (reg, res) => {
-    const { id } = reg.params;
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
 
-    const result = await Contact.findByIdAndUpdate(id, reg.body);
-    if (!result) 
-    {
-        throw HttpError(404, `Contact with ${id} not found`);
-    }
-    res.json(result);
+  const result = await Contact.findOneAndUpdate({ _id: id, owner }, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
 };
 
-const updateStatusContact = async (reg, res) => {
-    const { id } = reg.params;
-
-    const result = await Contact.findByIdAndUpdate(id, reg.body);
-    if (!result) 
-    {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findOneAndUpdate({ _id: id, owner }, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
 };
 
 const removeContact = async (req, res) => {
-    const { id } = req.params;
-    const removedContact = await Contact.findByIdAndRemove(id);
-    if (!removedContact) 
-    {
-        throw HttpError(404, "Not found");
-    }
-    res.status(200).json({ message: "contact deleted" });
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const removedContact = await Contact.findOneAndRemove({ _id: id, owner });
+  if (!removedContact) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({ message: "contact deleted" });
 };
 
 module.exports = {
-    getAll: ctrlWrapper(getAll),
-    getById: ctrlWrapper(getById),
-    add: ctrlWrapper(add),
-    updateById: ctrlWrapper(updateById),
-    updateStatusContact: ctrlWrapper(updateStatusContact),
-    removeContact: ctrlWrapper(removeContact),
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  removeContact: ctrlWrapper(removeContact),
 };
--- models/hooks.js
+++ models/hooks.js
@@ -1,10 +1,11 @@
 module.exports.handleSaveError = (error, data, next) => {
-    error.status = 400;
-    next();
+  const { name, code } = error;
+  error.status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  next();
 };
 
 module.exports.runValidatorsAtUpdate = function (next) {
-    this.options.runValidators = true;
-    this.options.new = true;
-    next();
+  this.options.runValidators = true;
+  this.options.new = true;
+  next();
 };
