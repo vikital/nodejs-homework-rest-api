6dea41a973488ecf81cc559c1532518ee9461b40
vitalyon <vitalyon@gmail.com>
2023-10-29 18:24:19 +0100

    avatar_1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,77 +1,111 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+
 const { User } = require("../models/user");
+
 const HttpError = require("../helpers/HttpError");
+
 const ctrlWrapper = require("../decorators/ctrlWrapper");
+
+const avatarsPath = path.resolve("public", "avatars");
+
 const { JWT_SECRET } = process.env;
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) 
-    {
-        throw HttpError(409, "Email in use");
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10);
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
-    res.status(201).json({
-        user: {
-            email: newUser.email,
-            subscription: newUser.subscription,
-        },
-    });
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
 };
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) 
-    {
-        throw HttpError(401, "Email or password is wrong");
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if (!passwordCompare) 
-    {
-        throw HttpError(401, "Email or password is wrong");
-    }
-    const payload = {
-        id: user._id,
-    };
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
-    await User.findByIdAndUpdate(user._id, { token });
-    res.json({
-        token,
-        user: {
-            email: user.email,
-            subscription: user.subscription,
-        },
-    });
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
 };
 
 const getCurrent = async (req, res) => {
-    const { email, subscription } = req.user;
+  const { email, subscription } = req.user;
 
-    res.json({
-        email,
-        subscription,
-    });
+  res.json({
+    email,
+    subscription,
+  });
 };
 
 const logout = async (req, res) => {
-    const { _id } = req.user;
+  const { _id } = req.user;
 
-    await User.findByIdAndUpdate(_id, { token: "" });
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-    res.status(204).send();
+  res.status(204).send();
 };
 
+const updateAvatar = async (req, res, next) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsPath, filename);
+  await fs.rename(tempUpload, resultUpload);
+
+  try {
+    const avatar = await Jimp.read(resultUpload);
+    await avatar.resize(250, 250).writeAsync(resultUpload);
+
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({
+      avatarURL,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
